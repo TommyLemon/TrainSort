dfbfb35e0c8671981380f33dec508d5adc988000
TommyLemon <1184482681@qq.com>
2020-04-30 18:00:46 +0800

    比较次数降到和冒泡排序一样；新增完全顺序和完全倒序的测试用例；删除多余的 Idea 自动生成的配置文件

--- src/TrainSort.java
+++ src/TrainSort.java
@@ -34,6 +34,11 @@ public class TrainSort {
         assertEquals(trainCrossingSort(new int[]{}), new int[]{});
         assertEquals(trainCrossingSort(new int[]{1}), new int[]{1});
 
+        assertEquals(trainCrossingSort(new int[]{1, 2}), new int[]{1, 2});
+        assertEquals(trainCrossingSort(new int[]{2, 1}), new int[]{1, 2});
+        assertEquals(trainCrossingSort(new int[]{-3, -2, -1}), new int[]{-3, -2, -1});
+        assertEquals(trainCrossingSort(new int[]{-1, -2, -3}), new int[]{-3, -2, -1});
+
         assertEquals(trainCrossingSort(new int[]{ 1, -6, 12, 0, -3 }), new int[]{-6, -3, 0, 1, 12});
         assertEquals(trainCrossingSort(new int[]{ 2, -1, 7, 9, 0, 120, -33 }), new int[]{-33, -1, 0, 2, 7, 9, 120});
 
@@ -60,33 +65,35 @@ public class TrainSort {
         int compareCount = 0;
 
         if (arr != null && arr.length > 1) {
-            for (int i = 0; i < 2 * arr.length; i++) {  //头对尾 -> 尾对头
-                //            System.out.println("  <<<<<<<<<<< trainCrossingSort  for i = " + i + " {");
+            for (int i = 0; i < 2 * arr.length - 1; i++) {  //头对尾 -> 尾对头
+//                            System.out.println("  <<<<<<<<<<< trainCrossingSort  for i = " + i + " {");
 
-                for (int j = 0; j <= i; j++) {  //重叠部分车厢，一对一对比
+                int innerSize = i%arr.length; //完全正对时，自己比较自己没有意义 (i + 1)%arr.length;
+                //boolean isClosing = i <arr.length;
+
+                for (int j = 0; j <= innerSize; j++) {  //重叠部分车厢，一对一对比
                     compareCount++;
-                    //                System.out.println("   <<<<< trainCrossingSort  for j = " + j + " {");
+//                                    System.out.println("   <<<<< trainCrossingSort  for j = " + j + " {");
 
-                    int k = j - 1 - i + arr.length;
+                    int k = j - 1 - innerSize + arr.length;
 
-                    //                System.out.print("    trainCrossingSort i = " + i + "; j = " + j + "; k = " + k + "; compareCount = " + compareCount + " >> ");
-                    if (k >= 0 && k < arr.length && j < arr.length) {  //排除未重叠的车厢
-                        //                    System.out.println("      trainCrossingSort  k >= 0 && k < arr.length && j < arr.length >> ");
+//                                    System.out.print("    trainCrossingSort i = " + i + "; j = " + j + "; k = " + k + "; compareCount = " + compareCount + " >> ");
+//                                            System.out.println("      trainCrossingSort  k >= 0 && k < arr.length && j < arr.length >> ");
 
-                        if ((j < k && arr[j] > arr[k]) || (j > k && arr[j] < arr[k])) {
-                            //                        System.out.println("        trainCrossingSort  (j < k && arr[j] > arr[k]) || (j > k && arr[j] < arr[k]) >> swap: arr[" + j + "] = " + arr[j] + ", arr[" + k + "] = " + arr[k]);
-                            int temp = arr[j];
-                            arr[j] = arr[k];
-                            arr[k] = temp;
-                        }
+//                        if ((j < k && arr[j] > arr[k]) || (j > k && arr[j] < arr[k])) {
+                    if (arr[j] > arr[k]) {  //先靠近，后远离，靠近时 (j < k && arr[j] > arr[k])，远离时 (j > k && arr[j] < arr[k])
+                        //                        System.out.println("        trainCrossingSort  (j < k && arr[j] > arr[k]) || (j > k && arr[j] < arr[k]) >> swap: arr[" + j + "] = " + arr[j] + ", arr[" + k + "] = " + arr[k]);
+                        int temp = arr[j];
+                        arr[j] = arr[k];
+                        arr[k] = temp;
 
-                        //                    System.out.println("      trainCrossingSort  swap >> arr = " + toString(arr));
+//                            System.out.println("      trainCrossingSort  swap >> arr = " + toString(arr));
                     }
 
-                    //                System.out.println("   >>>>> trainCrossingSort  for j = " + j + " }\n");
+//                                    System.out.println("   >>>>> trainCrossingSort  for j = " + j + " }\n");
                 }
 
-                //            System.out.println(" >>>>>>>>>>>> trainCrossingSort  for i = " + i + " } \n\n");
+//                            System.out.println(" >>>>>>>>>>>> trainCrossingSort  for i = " + i + " } \n\n");
             }
         }
 
